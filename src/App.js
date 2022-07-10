0f8556a57b65459fa0415b70ebe1dafa7e603282
keerthi46 <keerthiraj143.kr@gmail.com>
2022-07-10 18:10:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,120 +3,119 @@ import FormLabel from "./components/FormLabel";
 import FormTextInput from "./components/FormTextinput";
 import FormButton from "./components/FormButton";
 import FormValidationMessage from "./components/FormValidationMessage";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const initialValues = { username: "", email: "", password: "" };
-  const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
+  const initialState = {
+    Username: "",
+    Email: "",
+    Password: "",
   };
 
-  const handleSubmit = (e) => {
+  const [formData, setFormData] = useState(initialState);
+  const [formError, setFormError] = useState(null);
+  const { Username, Email, Password } = formData;
+
+  const onSubmit = (e) => {
+    console.log(formData);
     e.preventDefault();
-    setFormErrors(validate(formValues));
-    setIsSubmit(true);
+    setFormError(null);
+
+    let error = validateForm();
+    if (error.errorUsername || error.errorPassword || error.errorEmail) {
+      setFormError(error);
+
+      console.log("validation failed,form not submitted");
+      return;
+    }
+    console.log("validation successful,form submitted");
   };
 
-  useEffect(() => {
-    console.log(formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues);
-    }
-  }, [formErrors]);
-  const validate = (values) => {
-    const errors = {};
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+  const validateForm = () => {
+    let errorUsername = false;
+    let errorEmail = false;
+    let errorPassword = false;
 
-    if (!values.username) {
-      errors.username = "Username is required!";
+    if (Username === "") {
+      errorUsername = {
+        field_id: "Username",
+        message: "Username is required!",
+      };
+      // return error;
     }
-    if (!values.email) {
-      errors.email = "Email is required!";
+
+    if (Email === "") {
+      errorEmail = {
+        field_id: "Email",
+        message: "Email is required!",
+      };
+      // return error;
     }
-    if (!values.password) {
-      errors.password = "Password is required!";
+
+    if (Password === "") {
+      errorPassword = {
+        field_id: "Password",
+        message: "Password is required!",
+      };
+      // return { errorUsername, errorEmail, errorPassword };
     }
-    if (!values.email) {
-      errors.email = "Email is required!";
-    } else if (!regex.test(values.email)) {
-      errors.email = "This is not a valid email format!";
-    }
-    if (!values.password) {
-      errors.password = "Password is required!";
-    } else if (values.password.length < 4) {
-      errors.password = "Password must be more than 4 characters";
-    } else if (values.password.length > 10) {
-      errors.password = "Password cannot exceed more than 10 characters";
-    }
-    return errors;
+
+    return { errorUsername, errorEmail, errorPassword };
+  };
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    onSubmit(e);
   };
 
   return (
-    <div className="container">
-      {Object.keys(formErrors).length === 0 && isSubmit ? (
-        <div className="success">Signed in successfully</div>
-      ) : (
-        // <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-        <pre></pre>
-      )}
+    <div className="App">
+      <h1>Login Form</h1>
+      <form onSubmit={onSubmit}>
+        <FormLabel htmlFor="Username"> Username </FormLabel>
+        <FormTextInput
+          id="Username"
+          name="Username"
+          onChange={onChange}
+          value={Username}
+        />
+        {formError && formError.errorUsername.field_id === "Username" ? (
+          <FormValidationMessage>
+            {formError.errorUsername.message}
+          </FormValidationMessage>
+        ) : null}
 
-      <form onSubmit={handleSubmit}>
-        <h1>Login Form</h1>
-        <div className="ui divider"></div>
-        <div className="ui form">
-          {/*<div className="field">*/}
+        <FormLabel htmlFor="Email">Email</FormLabel>
+        <FormTextInput
+          id="Email"
+          name="Email"
+          onChange={onChange}
+          value={Email}
+        />
+        {formError && formError.errorEmail.field_id === "Email" ? (
+          <FormValidationMessage>
+            {formError.errorEmail.message}
+          </FormValidationMessage>
+        ) : null}
 
-          <label>Username</label>
+        <FormLabel htmlFor="Password">Password</FormLabel>
+        <FormTextInput
+          id="Password"
+          name="Password"
+          onChange={onChange}
+          value={Password}
+        />
+        {formError && formError.errorPassword.field_id === "Password" ? (
+          <FormValidationMessage>
+            {formError.errorPassword.message}
+          </FormValidationMessage>
+        ) : null}
 
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={formValues.username}
-            onChange={handleChange}
-          />
-
-          {/* </div> */}
-          <p>{formErrors.username}</p>
-          {/* <div className="field"> */}
-
-          <label>Email</label>
-
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            value={formValues.email}
-            onChange={handleChange}
-          />
-
-          {/* </div> */}
-          <p>{formErrors.email}</p>
-          {/* <div className="field"> */}
-
-          <label>Password</label>
-
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={formValues.password}
-            onChange={handleChange}
-          />
-
-          {/* </div> */}
-          <p>{formErrors.password}</p>
-
-          <FormButton primary>Login</FormButton>
-        </div>
+        <FormButton primary>Login</FormButton>
       </form>
     </div>
   );
 }
-
 export default App;
--- src/components/FormValidationMessage/index.jsx
+++ src/components/FormValidationMessage/index.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import "./style.css";
 
 function FormValidationMessage({ children }) {
-  return;
-  <div className="form-validation-message">{children}</div>;
+  return <div className="form-validation-message">{children}</div>;
 }
 
 export default FormValidationMessage;
